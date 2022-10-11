f25dffd94d067c328202e65e352c906dd27d4857
sjnay <samanthajnay@gmail.com>
2022-10-11 14:39:03 -0500

    frontend backend connected

--- frontend/fude/src/App.js
+++ frontend/fude/src/App.js
@@ -1,25 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState,useEffect} from 'react'
+
+
+
 
 function App() {
+const [feed,setFeed]=useState(null)
+
+const connectDB = ()=>{
+
+  fetch("http://localhost:8000/api/posts/?format=json")
+  .then((res)=>(res.json()))
+  .then((json)=>{
+    setFeed(json)
+    console.log(json)
+  })
+}
+
+useEffect(connectDB,[])
+
+console.log(feed[0].place_name)
+
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <p>{feed[0].place_name}</p>
+   <img src={feed[0].food_img} alt={feed[0].id}></img>
     </div>
   );
 }
+
 
 export default App;
